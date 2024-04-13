8a067c1dbba0431fed01517a8c12a4e2b69c43c5
andreidrobota <drobotaandrei@gmail.com>
2024-04-13 07:59:38 +0000

    Added downloadable CV, and contact form

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -14,8 +14,10 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print ("Hello is anybody there?")
     return render_template("contact.html")
 
 
